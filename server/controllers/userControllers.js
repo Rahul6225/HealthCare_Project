ebe1638cf1ae3376adce319f5141f948d07daf81
Rahul6225 <rahul.thakurbk3000@gmail.com>
2024-11-20 21:37:19 +0530

    "new Changes made"

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -69,13 +69,34 @@ const loginUser = asyncHandler(async(req, res) => {
 const getUserProfile = asyncHandler(async(req,res)=>{
     try{
         const email = req.body;
-        const data = await user.findOne(email);
+        const data = await User.findOne(email);
         if(!data) return res.status(401).json({err});
          return res.status(200).json({data});
     }
     catch{
         return res.status(500).json({err});
     }
+
+});
+const updateuserProfile = asyncHandler(async(req,res)=>{
+    try{
+        const {username,email,phoneNum} = req.body;
+        const userId = req.user.id;
+        const updateuser = await user.findByIdAndUpdate(
+            userId,{
+                username,
+                email,
+                password
+            },{new:true,runValidator:true}
+
+        ).select("password");
+        if(!updateuser) return res.status(404).json({message:"User not found"});
+        return res.status(200).json({message:"User Update Successfully",user:updateuser});
+}
+    catch{
+        return res.status(500).json({message:"Server Error"});
+    }
+        
 
 });
 module.exports = { registerUser , loginUser};
